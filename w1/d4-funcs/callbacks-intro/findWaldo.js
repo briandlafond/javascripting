98a506dca8688acb9c0eb0f54c9197156ef34ddd
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-04 18:12:42 +0000

    refactored for forEach

---
+++ w1/d4-funcs/callbacks-intro/findWaldo.js
@@ -0,0 +1,17 @@
+// The second argument / parameter is expected to be a(callback)
+// function
+
+const findWaldo = function(names, found) {
+
+    names.forEach((name, index) => {
+        if (name === "Waldo") {
+            found(index); // execute callback
+        }
+    })
+}
+
+const actionWhenFound = function(index) {
+    console.log("Found him at index " + index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
